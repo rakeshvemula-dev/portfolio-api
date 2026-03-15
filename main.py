4c3c52f51bb048b21ba6134598cf4fb424bc7711
Rakesh Vemula <your-email@example.com>
2026-03-15 22:39:15 +0530

    Update CORS allowed origin to specify full live portfolio URL

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ app.add_middleware(
     allow_origins=[
         "http://127.0.0.1:5500",
         "http://localhost:5500",
-        "https://rakeshvemula-dev.github.io",  # your live portfolio
+        "https://rakeshvemula-dev.github.io/portfolio/",  # your live portfolio
     ],
     allow_credentials=True,
     allow_methods=["*"],
